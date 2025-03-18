07a207a74b0cd4d1e12a9267eb5a14d3a8b88bb6
Jonathan Cruz <jonathan.cruz@davinci.edu.ar>
2025-03-18 10:49:37 -0300

    Clase 01

--- semana_1/Persona.js
+++ semana_1/Persona.js
@@ -3,6 +3,12 @@ class Persona {
     nombre = '';
     apellido = '';
     edad = 0;
+
+    constructor(nombre, apellido, edad){
+        this.nombre =  nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
     // Métodos
     getNombre = function(){
         return this.nombre;
