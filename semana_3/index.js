f4b463bc9567f9178d1ac12ab5edf4870df9729e
Jonathan Cruz <jonathan.cruz@davinici.edu.ar>
2025-04-01 10:26:52 -0300

    Semana 03 - Introducción a Express

--- semana_3/index.js
+++ semana_3/index.js
@@ -1,8 +1,29 @@
 import chalk from "chalk";
-
+import express from "express";
 const port = 5000;
+const app = express();
+
+app.get('/', (request, response) =>{
+
+    console.log('Ruta Raíz');
+    response.send('Home');
+})
+
+app.get('/products', (requets, response ) => {
+    const lista = [ { id: 1, name:'Mouse', price: 23000} ];
+    response.json( lista );
+})
 
 
-console.log(   chalk.green('Servidor Web')  );
+app.post('/products', (request, response) =>{
+    console.log('POST');
+    response.json({ id: 2});
+})
 
-console.log(  chalk.bgCyan('Puerto ' + port)  );
+
+app.listen( port, () => {
+    console.log(   chalk.green(`Servidor Web en el puerto ${port}`)  );    
+})
+
+
+
