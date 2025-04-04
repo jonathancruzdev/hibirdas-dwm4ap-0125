b2a67d21629a0398402d4409f1a8546619941f5f
Jonathan Cruz <jonathan.cruz@davinci.edu.ar>
2025-04-04 10:11:39 -0300

    Semana 03 - Express

---
+++ semana_3/UsersManager.js
@@ -0,0 +1,67 @@
+import fs from "fs/promises";
+const path = './users.json';
+
+class UsersManager{
+    users = [];
+    constructor(users=[]){
+        this.users = users
+    }
+    randomID(){
+        return crypto.randomUUID();
+    }
+    async addUser(user){
+        await this.getUsers();
+        user.id = this.randomID();
+        this.users.push( user );
+        
+        const data = JSON.stringify( this.users, null, 2);
+        try {
+            await fs.writeFile( path, data );
+            return user.id;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    async getUsers(){
+        try {
+            const data = await fs.readFile(path);
+            this.users = JSON.parse( data );
+            return this.users;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    async getUserById(id){
+        const users = await this.getUsers();
+        const user = users.find( u => u.id == id );
+        return user ? user : undefined;
+    }
+    async deleteUserById(id){
+        await this.getUsers();
+        const pos = this.users.findIndex( u => u.id == id);
+        this.users.splice(pos, 1);
+
+        const data = JSON.stringify( this.users, null, 2);
+        try {
+            await fs.writeFile( path, data );
+            return true ? pos != -1 : false;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    async updateUserById (id, user){
+        await this.getUsers();
+        const pos = this.users.findIndex( u => u.id == id);
+        this.users[pos].name = user.name;
+        this.users[pos].email = user.email;
+        const data = JSON.stringify( this.users, null, 2);
+        try {
+            await fs.writeFile( path, data );
+            return user.id;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
+
+export default UsersManager;
--- semana_3/index.js
+++ semana_3/index.js
@@ -2,15 +2,16 @@ import chalk from "chalk";
 import express from "express";
 //import { ProductManger } from "./ProductManager.js";
 import ProductManger from "./ProductManager.js";
-
+import UsersManager from "./UsersManager.js";
 
 const port = 5000;
 const app = express();
-const admin = new ProductManger();
 
+app.use(express.json());
+const admin = new ProductManger();
+const userModel = new UsersManager();
 
 app.get('/', (request, response) =>{
-
     console.log('Ruta Raíz');
     response.send('Home');
 })
@@ -26,6 +27,48 @@ app.post('/products', (request, response) =>{
     response.json({ id: 2});
 })
 
+
+// Rutas para los usuarios
+app.get('/api/users', async (request, response) => {
+    const users = await userModel.getUsers();
+    console.log(users);
+    response.json( users );
+})
+
+app.get('/api/users/:id', async (request, response) => {
+    const id = request.params.id;
+    const user = await userModel.getUserById(id);
+    if ( user) {
+        response.json( user );
+    } else {
+        response.status(404).json({msg: 'No se encontro el usuario'});
+    }
+})
+
+app.post('/api/users', async (request, response) => {
+    const user = request.body;
+
+    console.log({user});
+
+    const id = await userModel.addUser(user);
+   
+    response.json( { id} );
+})
+
+
+app.delete('/api/users/:id', async (request, response) => {
+    const id = request.params.id;
+    const status = await userModel.deleteUserById(id);
+    if ( status) {
+        response.json( {msg: 'Usuarios eliminado'} );
+    } else {
+        response.status(404).json({msg: 'No se encontro el usuario'});
+    }
+})
+
+app.put('/api/users/:id', async (request, response)=>{
+
+})
 
 app.listen( port, () => {
     console.log(   chalk.green(`Servidor Web en el puerto ${port}`)  );    
