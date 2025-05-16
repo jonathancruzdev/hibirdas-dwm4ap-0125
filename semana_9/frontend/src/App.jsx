40f37bb0a785f04c014f87c4409a09637c819657
Jonathan Cruz <jonathan.cruz@davinci.edu.ar>
2025-05-16 10:48:23 -0300

    Semana 09 - React - componentes

--- semana_9/frontend/src/App.jsx
+++ semana_9/frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import viteLogo from '/vite.svg'
 import logo from '/component.png'
 import './App.css'
-
+import Card from './components/Card';
 function App() {
 
   let nombre = 'Juan';
@@ -9,14 +9,25 @@ function App() {
   let activo = true;
   const detalleLogo = 'Logo de la aplicación';
   const detalle = <h4> React es una librería de JS</h4>;
-
-  console.log(logo);
+  const objeto = {
+    cantidad: 100,
+    descuento: 10
+  }
   return (
     <>
       <h1 className="info"> Semana 09</h1>
       <p>Sintaxis JSX</p>
 
-      <img src="component.png" alt="foto" title={detalleLogo} />
+      <Card precio={54} nombre="Teclado RGB" 
+        datos={objeto} descripcion="Teclado mecanico RGB..." foto="https://http2.mlstatic.com/D_NQ_NP_2X_636746-MLA52350707355_112022-F.webp"></Card>
+
+      <Card 
+        precio={25} 
+        nombre="Mouse" 
+        datos={objeto}
+      />
+
+      <img src="component.png" alt="foto" title={detalleLogo}  />
 
       <img src={logo} alt="Logo" />
 
---
+++ semana_9/frontend/src/components/Card.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import '../App.css';
+
+function Card( { nombre, precio, foto, datos, descripcion='...' } ) {
+    // JavaScript
+/*     console.log(props.nombre);
+    const nombre = props.nombre;
+    const descripcion = props.descripcion ? props.descripcion : 'Sin descripciòn';
+    const foto = props.foto;
+ */
+   console.log(datos)
+
+    // HTML
+    return (
+        <div className="card">
+            <h4> { nombre }</h4>
+            <img src={foto} alt={nombre} />
+            <p>$ { precio + 2 }</p>
+            <p>Stock {  datos?.cantidad }</p>
+            <p> { descripcion }</p>
+            <button type="button">Ver</button>
+        </div>
+    )
+}
+
+export default Card;
